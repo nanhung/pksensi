c71d87e9c8003f9ad6a7e4d85a5c9b56a5874dbd
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2018-05-10 11:14:42 -0500

    Update rfast99.R

--- R/rfast99.R
+++ R/rfast99.R
@@ -147,21 +147,21 @@ pksim <- function(y, log = F, legend = T, ...){
   }
   
   col.transp = adjustcolor('black', alpha = 0.2)
-  plot(time, quantY[1,], type="l", xlab="", ylab="",
+  plot(times, quantY[1,], type="l", xlab="", ylab="",
        ylim=c(min(ytck),max(ytck)), lty=1, las=1, lwd=2, col = 1, ...)
   if (any(is.na(quantY[2:3,])) == FALSE)
   {
-    polygon(x = c(times, rev(times)), y = c(quantY[2,],quantY[3,seq(from=length(time),to=1,by=-1)]),col=col.transp,lty=0)
+    polygon(x = c(times, rev(times)), y = c(quantY[2,],quantY[3,seq(from=length(times),to=1,by=-1)]),col=col.transp,lty=0)
   }
   if (any(is.na(quantY[4:5,])) == FALSE)
   {
-    polygon(x = c(times, rev(times)), y = c(quantY[4,],quantY[5,seq(from=length(time),to=1,by=-1)]),col=col.transp,lty=0)    
+    polygon(x = c(times, rev(times)), y = c(quantY[4,],quantY[5,seq(from=length(times),to=1,by=-1)]),col=col.transp,lty=0)    
   }
   if (any(is.na(quantY[6:7,])) == FALSE)
   {
-    polygon(x = c(times, rev(times)), y = c(quantY[6,],quantY[7,seq(from=length(time),to=1,by=-1)]),col=col.transp,lty=0)
+    polygon(x = c(times, rev(times)), y = c(quantY[6,],quantY[7,seq(from=length(times),to=1,by=-1)]),col=col.transp,lty=0)
   }
-
+  
   if (legend == T){
     legend('topright', # inset=c(0,1), xpd=TRUE, horiz=TRUE,
            legend = c('Median', '25%-75%', '10%-90%', 'min-max'), 
