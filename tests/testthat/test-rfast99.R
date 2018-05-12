3293a1ceaef5987c71f8a451653c026340f01f6c
nanhung <nhsieh@cvm.tamu.edu>
2018-05-12 16:19:19 -0500

    test

--- tests/testthat/test-rfast99.R
+++ tests/testthat/test-rfast99.R
@@ -1,5 +1,11 @@
 context("test-rfast99.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("rfast99 class", {
+  output <- rfast99(factors = 20, n = 100, q = "qunif", q.arg = list(min = 0, max = 1), replicate = 20, conf = 0.95)
+  expect_that(output, is_a("rfast99"))
+  expect_equal(output$M, 4)
+  expect_equal(output$replicate, 20)
+  expect_equal(output$conf, 0.95)
+  expect_true(class(output$a) == "array")
+  expect_true(class(output$omega) == "numeric")
 })
