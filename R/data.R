c82852454d23eeec86ab415d64e7267362ccaa00
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-01-26 12:02:26 -0600

    add APAP dataset

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Pharmacokinetics of Acetaminophen
+#'
+#' The APAP dataset contains an human experiment of pharmacokinetic data
+#' for acetaminophen and it's major metabolites.
+#'
+#' @format A data frame containing the following columns:
+#' \itemize{
+#'   \item Wt: averaged weight of the study population (kg).
+#'   \item Dose: given oral dose of acetaminophen administered (mg/kg).
+#'   \item Time: time after drug administration (hr).
+#'   \item APAP: concentration of acetaminophen in the sample (mg/L).
+#'   \item AG: concentration of acetaminophen-glucuronide in the sample (mg/L).
+#'   \item AS: concentration of acetaminophen-sulfate in the sample (mg/L).
+#'   \item Study: Sourced reference.
+#' }
+"APAP"
