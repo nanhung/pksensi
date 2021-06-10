47de148aa034648f4b812b91a3eb5f411ab20913
Nan-Hung Hsieh <nan_1003@hotmail.com>
2021-06-09 21:49:30 -0700

    test-mcsim

---
+++ tests/testthat/test-mcsim.R
@@ -0,0 +1,6 @@
+context("test-mcsim.R")
+
+test_that("mcsim version", {
+  mcsim_version()
+  pksensi:::generate_config.h()
+})
--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -3,6 +3,8 @@ context("test-models.R")
 test_that("models pbtk1cpt_model", {
 
   pbtk1cpt_model()
+  compile_model("pbtk1cpt")
+  compile_model("pbtk1cpt", application = 'R')
   expect_true(file.exists("pbtk1cpt.model"))
 
 })
